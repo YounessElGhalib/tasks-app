fdcf0a85966869187e96cf0de12bd7f5768efa8f
Youness El Ghalib <youness.elghalib@promy.fr>
2023-08-17 16:31:12 +0100

    save vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/tasks-app/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/tasks-app/" : "/ ",
   transpileDependencies: true
 })
